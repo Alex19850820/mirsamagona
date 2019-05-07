4d0ea5c4d19233a5ab14be441a7e8a31792501f0
Александр <shlykovn@mail.ru>
2019-05-07 23:38:24 +0300

    controller product

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -224,7 +224,7 @@ class ControllerProductCategory extends Controller {
 				$data['products'][] = array(
 					'product_id'  => $result['product_id'],
 					'thumb'       => $image,
-					'name'        => $result['name'],
+					'name'        => utf8_substr($result['name'], 0, 65). '..',
 					'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..',
 					'price'       => $price,
 					'special'     => $special,
